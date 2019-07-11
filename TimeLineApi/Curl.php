25d51930436aa5c28f6352855a879e93f43abab0
sameekshaHCL <50136702+sameekshaHCL@users.noreply.github.com>
2019-07-11 11:35:36 +0530

    Update_new_code

--- TimeLineApi/Curl.php
+++ TimeLineApi/Curl.php
@@ -2,39 +2,46 @@
 /*
 # Author: Sameeksha Agrawal
 # Date Created:  04/07/2019
-# This class is the base class to make connection with curl
+# This class is the base class to make connection with curl 
 */
-require_once('config.php');
-require_once('DBOpertaions.php');
+require_once 'config.php';
+require_once 'DBOpertaions.php';
 
 trait Curl
 {
-        /**
-        * Make Curl Connect
-        **/
+	
+	/**
+	* Make Curl Connect
+	**/
+	public function curlCall($curl_url , $http_header, $payload, $method)
+	{ 
+		$auth_curl_request = curl_init();
+		if ($method == "POST" || $method == "PUT") {
+			$json__payload = json_encode($payload);
+			if($method == "PUT") {
+				curl_setopt($auth_curl_request, CURLOPT_CUSTOMREQUEST, "PUT");
+			}
+			curl_setopt($auth_curl_request, CURLOPT_POSTFIELDS, $json__payload);
+		}
+		curl_setopt($auth_curl_request, CURLOPT_URL, $curl_url);
+		curl_setopt($auth_curl_request, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+		curl_setopt($auth_curl_request, CURLOPT_HEADER, false);
+		curl_setopt($auth_curl_request, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($auth_curl_request, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($auth_curl_request, CURLOPT_FOLLOWLOCATION, 0);
+		curl_setopt($auth_curl_request, CURLOPT_HTTPHEADER, $http_header);
 
-        public function curlCall($curl_url , $http_header, $payload, $method)
-        {
-                $auth_curl_request = curl_init();
-                if ($method == "POST" || $method == "PUT") {
-                        $json__payload = json_encode($payload);
-                        if($method == "PUT") {
-                                curl_setopt($auth_curl_request, CURLOPT_CUSTOMREQUEST, "PUT");
-                        }
-                        curl_setopt($auth_curl_request, CURLOPT_POSTFIELDS, $json__payload);
-                }
-                curl_setopt($auth_curl_request, CURLOPT_URL, $curl_url);
-                curl_setopt($auth_curl_request, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
-                curl_setopt($auth_curl_request, CURLOPT_HEADER, false);
-                curl_setopt($auth_curl_request, CURLOPT_SSL_VERIFYPEER, 0);
-                curl_setopt($auth_curl_request, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($auth_curl_request, CURLOPT_FOLLOWLOCATION, 0);
-                curl_setopt($auth_curl_request, CURLOPT_HTTPHEADER, $http_header);
-
-                //execute request
-                $curl_response = curl_exec($auth_curl_request);
-                //print_R($curl_response);
-                return $curl_response;
-        }
-
+		//execute request
+		
+		$curl_response = curl_exec($auth_curl_request);
+		
+		if (curl_errno($auth_curl_request)) {
+			$response = array(
+					"curlStatus" => "Fail",
+					"msg" => "Something went wrong. Please try again !"
+					);
+			$curl_response = json_encode($response);
+		}
+		return $curl_response;
+	}
 }
